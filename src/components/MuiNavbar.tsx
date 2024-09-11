1f9b3c0f92fff330060c91b30242ef05c56b2db7
unknown <terbagdasarovartur@gmail.com>
2024-09-11 20:30:15 +0400

    Add isVertical on MuiNavigation

--- src/components/MuiNavbar.tsx
+++ src/components/MuiNavbar.tsx
@@ -18,12 +18,12 @@ import { Outlet, useNavigate } from 'react-router-dom';
 const settings = ['Profile', 'Logout'];
 
 interface MuiNavbarProps {
-    routes: IRoute[],
-    isVertical: boolean,
+  routes: IRoute[];
+  isVertical: boolean;
 }
 
-const MuiNavbar:React.FC<MuiNavbarProps> = ({routes}) => {
-  const navigate = useNavigate()
+const MuiNavbar: React.FC<MuiNavbarProps> = ({ routes, isVertical }) => {
+  const navigate = useNavigate();
   const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(null);
   const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(null);
 
@@ -35,7 +35,7 @@ const MuiNavbar:React.FC<MuiNavbarProps> = ({routes}) => {
   };
 
   const handleCloseNavMenu = (path: string) => {
-    navigate(path)
+    navigate(path);
     setAnchorElNav(null);
   };
 
@@ -45,90 +45,107 @@ const MuiNavbar:React.FC<MuiNavbarProps> = ({routes}) => {
 
   return (
     <>
-        <AppBar position="static">
+      <AppBar position="static">
         <Container maxWidth="xl">
-            <Toolbar disableGutters>
+          <Toolbar disableGutters>
             <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1 }} />
-
-            <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-                <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="menu-appbar"
-                aria-haspopup="true"
-                onClick={handleOpenNavMenu}
-                color="inherit"
-                >
-                <MenuIcon />
-                </IconButton>
-                <Menu
-                id="menu-appbar"
-                anchorEl={anchorElNav}
-                anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                keepMounted
-                transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'left',
-                }}
-                open={Boolean(anchorElNav)}
-                onClose={handleCloseNavMenu}
-                sx={{ display: { xs: 'block', md: 'none' } }}
-                >
-                {routes.map(({path, label}) => (
-                    <MenuItem key={path} onClick={() => handleCloseNavMenu(path)}>
-                    <Typography sx={{ textAlign: 'center' }}>{label}</Typography>
-                    </MenuItem>
-                ))}
-                </Menu>
-            </Box>
-            <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
-                {routes.map(({path, label}) => (
-                <Button
+            {isVertical ? (
+              <Box sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+                {routes.map(({ path, label }) => (
+                  <Button
                     key={path}
                     onClick={() => handleCloseNavMenu(path)}
-                    sx={{ my: 2, color: 'white', display: 'block' }}
-                >
+                    sx={{ my: 1, color: 'white', display: 'flex', textAlign: 'left', flexDirection: 'center' }}
+                  >
                     {label}
-                </Button>
+                  </Button>
                 ))}
-            </Box>
+              </Box>
+            ) : (
+              <>
+                <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+                  <IconButton
+                    size="large"
+                    aria-label="account of current user"
+                    aria-controls="menu-appbar"
+                    aria-haspopup="true"
+                    onClick={handleOpenNavMenu}
+                    color="inherit"
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Menu
+                    id="menu-appbar"
+                    anchorEl={anchorElNav}
+                    anchorOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    keepMounted
+                    transformOrigin={{
+                      vertical: 'top',
+                      horizontal: 'left',
+                    }}
+                    open={Boolean(anchorElNav)}
+                    onClose={() => setAnchorElNav(null)}
+                    sx={{ display: { xs: 'block', md: 'none' } }}
+                  >
+                    {routes.map(({ path, label }) => (
+                      <MenuItem key={path} onClick={() => handleCloseNavMenu(path)}>
+                        <Typography sx={{ textAlign: 'center' }}>{label}</Typography>
+                      </MenuItem>
+                    ))}
+                  </Menu>
+                </Box>
+                <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+                  {routes.map(({ path, label }) => (
+                    <Button
+                      key={path}
+                      onClick={() => handleCloseNavMenu(path)}
+                      sx={{ my: 2, color: 'white', display: 'block' }}
+                    >
+                      {label}
+                    </Button>
+                  ))}
+                </Box>
+              </>
+            )}
+
             <Box sx={{ flexGrow: 0 }}>
-                <Tooltip title="Open settings">
+              <Tooltip title="Open settings">
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                    <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+                  <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
                 </IconButton>
-                </Tooltip>
-                <Menu
+              </Tooltip>
+              <Menu
                 sx={{ mt: '45px' }}
                 id="menu-appbar"
                 anchorEl={anchorElUser}
                 anchorOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 keepMounted
                 transformOrigin={{
-                    vertical: 'top',
-                    horizontal: 'right',
+                  vertical: 'top',
+                  horizontal: 'right',
                 }}
                 open={Boolean(anchorElUser)}
                 onClose={handleCloseUserMenu}
-                >
+              >
                 {settings.map((setting) => (
-                    <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                  <MenuItem key={setting} onClick={handleCloseUserMenu}>
                     <Typography sx={{ textAlign: 'center' }}>{setting}</Typography>
-                    </MenuItem>
+                  </MenuItem>
                 ))}
-                </Menu>
+              </Menu>
             </Box>
-            </Toolbar>
+          </Toolbar>
         </Container>
-        </AppBar>
-        <Outlet />
+      </AppBar>
+      <Outlet />
     </>
   );
-}
+};
+
 export default MuiNavbar;
